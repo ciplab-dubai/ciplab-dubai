21d32af7f2540d047b04339b4b40bf1e2839f14b
ciplab-dubai <amer.opswat@hotmail.com>
2025-04-09 12:38:05 +0400

    Create secrets.js
    Add dummy secrets file for DLP testing

---
+++ secrets.js
@@ -0,0 +1,15 @@
+// 🚨 This file contains fake secrets for testing DLP and secrets-scanning tools only
+// DO NOT use real credentials here.
+//Author: ?Amer
+
+const config = {
+  awsAccessKeyId: "AKIAFAKEKEY1234567890",
+  awsSecretAccessKey: "wJalrXUtnFakeSecretKey1234567890abcdEfgh",
+  githubToken: "ghp_fakeGithubPersonalAccessToken1234567890abcd",
+  slackWebhookUrl: "https://hooks.slack.com/services/T00000000/B00000000/FAKEslackWebHookToken",
+  jwtSecret: "supersecretjwtkeyforDLPtesting1234567890",
+  dbPassword: "P@ssw0rd123!",
+  stripeSecretKey: "sk_test_4eC39HqLyjWDarjtT1zdp7dc"
+};
+
+module.exports = config;
